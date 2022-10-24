364d3f1c296482fd45c7b7f5a698ccba995b3d5e
victorhrdsilva <victorhrdsilva@gmail.com>
2022-10-24 20:38:34 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+let user = '';
+let avatarUser = '';
+let messages = [];
+
+app.post('/sign-up', (req, res) => {
+    const {username, avatar} = req.body
+    user = username
+    avatarUser = avatar
+    
+    res.status(201);
+});
+
+app.post('/messages', (req, res) => {
+    messages.push(
+        {
+            username: user,
+            avatar: avatarUser, 
+            message: '' 
+        }
+    );
+    res.status(201);
+});
+  
+ app.get('/messages', (req, res) => {
+    res.send(pessoas);
+});
+
+app.listen(5000);
