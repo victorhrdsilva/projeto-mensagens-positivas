a86b030de9104fc4b058a6b57bdaa7eeef7cb9fc
victorhrdsilva <victorhrdsilva@gmail.com>
2022-10-24 22:18:47 -0300

    add messages and sign-up resquests

--- index.js
+++ index.js
@@ -5,31 +5,36 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-let user = '';
-let avatarUser = '';
+let users = [];
 let messages = [];
 
 app.post('/sign-up', (req, res) => {
     const {username, avatar} = req.body
-    user = username
-    avatarUser = avatar
+    users.push({
+        username: username,
+        avatar: avatar
+    })
     
-    res.status(201);
+    res.status(201).send("OK");
 });
 
 app.post('/messages', (req, res) => {
+    const {message, username} = req.body
+    let avatar = users.find(element => element.username === username);
+    avatar = avatar.avatar;
+
     messages.push(
         {
-            username: user,
-            avatar: avatarUser, 
-            message: '' 
+            username: username,
+            avatar: avatar, 
+            message: message 
         }
     );
-    res.status(201);
+    res.status(201).send("OK");
 });
   
- app.get('/messages', (req, res) => {
-    res.send(pessoas);
+app.get('/messages', (req, res) => {
+    res.send(messages.slice(0, 10));
 });
 
 
